94a1133eec8e8afdf3c015c7d8b51590c832c3bb
Michal Zak <mzak@localhost.localdomain>
2021-01-05 15:49:57 +0100

    added comments

--- insecure_deserialization/user.php
+++ insecure_deserialization/user.php
@@ -1,4 +1,6 @@
 <?php
+//https://medium.com/bugbountywriteup/demystifying-insecure-deserialization-in-php-684cab9c4d24
+//https://www.exit.wtf/vrd/2019/04/04/Insecure_Deserialization.html
 
     class User {
         private $username;
